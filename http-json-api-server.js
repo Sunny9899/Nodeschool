62dd951ca785571def54661e4543099e8300ee36
Sunny9899 <suryanshpajiala@gmail.com>
2022-04-25 10:15:02 +0530

    JSON Server API

--- http-json-api-server.js
+++ http-json-api-server.js
@@ -1,12 +1,15 @@
 const http = require("http");
-const path = require("path");
 const port = process.argv[2];
 
-http
-  .createServer((req, res) => {
+http.createServer((req, res) => {
 
+console.log(req.url);
+const response={
+  "hour":1,
+  "minute":2,
+  "second":3,
+}
+const sendresponse=JSON.stringify(response);
+res.end(sendresponse);
 
-
-  })
-  .listen(port);
-
+}).listen(port);
